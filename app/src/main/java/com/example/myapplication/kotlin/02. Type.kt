5c5da4e344427dac2ba12ba539d45390c8e438ca
hellominchan <wjdalscksdle@naver.com>
2020-10-13 14:14:39 +0900

    test2

---
+++ app/src/main/java/com/example/myapplication/kotlin/02. Type.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.kotlin
+
+var number = 10
+val hello1: String = "Hello"
+val point1: Double = 2.3
+
+fun main(array: Array<String>) {
+    number = 23
+    println(hello1)
+    println(point1)
+}
