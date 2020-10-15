3a1410d85233b6bb72187a42e65c8e5621a5e4cc
hellominchan <wjdalscksdle@naver.com>
2020-10-15 14:17:06 +0900

    test4

---
+++ app/src/main/java/com/example/myapplication/kotlin/06. Operator.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication.kotlin
+
+fun main(array: Array<String>) {
+    var a = 10 + 1
+    val b = 10 - 1
+    val c = 1 * 9
+    val d = 20 / 3
+    val e = 20 % 3
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+
+    val f = 5
+    a += 10
+    println(a++)
+    println(a)
+
+    println(a>b)
+    println(a<b)
+}
