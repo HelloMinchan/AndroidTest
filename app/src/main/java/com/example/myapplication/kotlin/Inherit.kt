2babac7f87c67ff358f95c715e2b870d920c9699
hellominchan <wjdalscksdle@naver.com>
2020-11-07 23:47:29 +0900

    test9

--- app/src/main/java/com/example/myapplication/kotlin/Inherit.kt
+++ app/src/main/java/com/example/myapplication/kotlin/Inherit.kt
@@ -2,15 +2,15 @@ package com.example.myapplication.kotlin
 
 fun main(args:Array<String>) {
     val superCar100 = SuperCar100()
-    superCar100.drive()
+    println(superCar100.drive())
     superCar100.stop()
 
     val bus100: Bus100 = Bus100()
 }
 
 open class Car100() {
-    fun drive () {
-
+    open fun drive (): String {
+        return "달린다"
     }
     fun stop () {
 
@@ -18,7 +18,10 @@ open class Car100() {
 }
 
 class SuperCar100(): Car100() {
-
+    override fun drive(): String {
+        val run = super.drive()
+        return "빨리ㅋㅋ $run"
+    }
 }
 
 class Bus100() {
