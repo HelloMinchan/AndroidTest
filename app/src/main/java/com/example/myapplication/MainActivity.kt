1c8c846bfb7de3066f649216589ebf5ab6468911
hellominchan <wjdalscksdle@naver.com>
2020-11-16 17:02:41 +0900

    youtube clone coding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.scroll)
+        setContentView(R.layout.youtube)
     }
 }
